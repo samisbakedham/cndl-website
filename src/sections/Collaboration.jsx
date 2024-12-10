668d61a37ac29f5728c49266651ab2465ee692a0
Samuel Safahi <60489622+samisbakedham@users.noreply.github.com>
2024-12-09 17:16:11 -0800

    Update Collaboration.jsx

--- src/sections/Collaboration.jsx
+++ src/sections/Collaboration.jsx
@@ -5,7 +5,7 @@ import Section from "../components/Section";
 const collabContent = [
     {
         title: "Seamless Integration",
-        text: "With smart automation and top-notch security, it's the perfect solution for teams looking to work smarter.",
+        text: "Candle’s blockchain integrates effortlessly with your existing workflows, delivering a unified and efficient experience.",
     },
     { title: "Smart Automation" },
     { title: "Top-notch Security" },
@@ -67,7 +67,7 @@ const Collaboration = () => {
         <Section crosses>
             <div className="container lg:flex">
                 <div className="max-w-[25rem]">
-                    <h2 className="h2 mb-4 md:mb-8">AI Chat App for seamless collaboration</h2>
+                    <h2 className="h2 mb-4 md:mb-8">Candle Blockchain for Seamless Collaboration</h2>
 
                     <ul className="max-w-[22rem] mb-10 md:mb-14">
                         {collabContent.map((item, i) => (
@@ -88,13 +88,15 @@ const Collaboration = () => {
                 </div>
 
                 <div className="lg:ml-auto xl:w-[38rem] mt-4">
-                    <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">With smart automation and top-notch security, it's the perfect solution for teams looking to work smarter.</p>
+                    <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+                        With powerful blockchain interoperability and top-notch security, Candle provides the ideal solution for teams seeking to work smarter and innovate faster.
+                    </p>
 
                     <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
                         <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
                             <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
                                 <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
-                                    <img src={openai} width={48} height={48} alt="OpenAI" />
+                                    <img src={openai} width={48} height={48} alt="Candle" />
                                 </div>
                             </div>
                         </div>
